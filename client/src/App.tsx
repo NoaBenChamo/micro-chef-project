ca70163417c1c940eba696222e84428e0c1f4136
noa ben <noa0534104310@gmail.com>
2026-09-09 18:24:23 +0300

    demo: test git flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,6 @@ import InventoryAnalytics from './pages/InventoryAnalytics'
 import AiRecipeDetails from './pages/AiRecipeDetails'
 import AppNavbar from './pages/AppNavbar'
 
-function func() {
-  throw new Error('Function not implemented.')
-}
-
 function Layout() {
   return (
     <>
